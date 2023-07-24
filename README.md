0ee3989a73b7b436a6fb89125089c65ccdb607e9
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-07-24 21:19:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Building Skia on a shoe-string budget
+
+The official procedure for building Skia tries to download ~3.5 GB and spats 7.1 GB+ onto your
+hard disk before even compiling the first source file. This is a script plus a patch set to
+do it with ~80MB download and ~300MB disk space usage, without compromising on any functionality.
+
+It takes about 40 minutes on dual-cores, or about 70 minutes on single CPU. Works for m116 and Linux
+only.
+
+Skia is facinating in both a bad and good way. The official build procedure does a 900MB clone,
+then fetch 1.6GB swiftshader (Mac OS X only), 1GB icu (available on Linux), 400MB angle2
+(used only for windows builds); that's 5.1GB of thirdparty stuff in total. Then it goes and
+fetch another 400 MB via `emsdk activate` and dump 1.1GB on the hard disk. That's only useful for
+building skia for WebAassembly. So it eats up 900MB + 5.1GB + 1.1GB ~ 7.1GB before
+you even start to build a single line.
+
+If you do shallow clone, only care about linux, and use system libraries as appropriate, you
+can cut the 7.1GB disk usage down to about 270MB. And the network usage from
+600MB + ~2.5GB + 400MB ~ 3.5GB to about 80MB download.
+
+Here is the script.
+
---
+++ download-and-build-skia.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright 2023 Hin-Tak Leung
+
+# currently only works for "m116"!
+
+VER=${1}
+
+# Shallow clones to avoid a 600MB+ download.
+git clone -b chrome/${VER} --depth 1 git@github.com:google/skia.git skia-${VER}
+
+pushd skia-${VER}/
+
+    # Stop uninteresting downloads, and disable emsdk
+    patch < ../patches/skia-${VER}-minimize-download.diff
+
+    # https://bugs.chromium.org/p/skia/issues/detail?id=14636
+    patch < ../patches/skia-${VER}-modules-symbols-svg.diff
+
+    # Official build process from here:
+    python tools/git-sync-deps
+
+    # is_component_build=true:    build shared libraries
+    # skia_enable_svg=true:       want the svg module
+    # - full-path clang/clang++   to avoid ccache
+    bin/gn gen out/Shared --args='is_official_build=true is_component_build=true skia_enable_svg=true cc="/usr/bin/clang" cxx="/usr/bin/clang++"'
+
+    # time the build, keep the log
+    /usr/bin/time -v ninja -C out/Shared/ 2>&1 | tee ../skia-${VER}-build-log-shared
+
+    # zip up the interesting part of outcome.
+    find out/Shared/ -type f -name '*.a' -or -name '*.so' >  ../skia-${VER}-bin-file-list
+    find include/ src/ modules/ -type f -name '*.h'       >> ../skia-${VER}-bin-file-list
+
+    cat ../skia-${VER}-bin-file-list | zip -@ ../skia-${VER}-bin.zip
+
+popd
+rm -rf skia-${VER}/
