a385d966f9f4cc311f2d577c1cec19d48f2c73ba
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-10-16 16:39:34 +0100

    m131 version of patches

--- download-and-build-skia.sh
+++ download-and-build-skia.sh
@@ -6,7 +6,7 @@
 
 ARG1=${1}
 
-VER=${ARG1:-m130}
+VER=${ARG1:-m131}
 
 # Shallow clones to avoid a 600MB+ download.
 git clone -b chrome/${VER} --depth 1 https://github.com/google/skia.git skia-${VER}
