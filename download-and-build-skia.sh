4600a06be50a6e7ca99247641d7a4e94bcff5512
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-11-13 18:53:53 +0000

    bump to m132

--- download-and-build-skia.sh
+++ download-and-build-skia.sh
@@ -6,7 +6,7 @@
 
 ARG1=${1}
 
-VER=${ARG1:-m131}
+VER=${ARG1:-m132}
 
 # Shallow clones to avoid a 600MB+ download.
 git clone -b chrome/${VER} --depth 1 https://github.com/google/skia.git skia-${VER}
