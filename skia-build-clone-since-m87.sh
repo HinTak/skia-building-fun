1afd5a741f919322a5734e7afc302f59bf67eb12
Hin-Tak Leung <htl10@users.sourceforge.net>
2025-05-03 14:45:28 +0100

    git gc

--- skia-build-clone-since-m87.sh
+++ skia-build-clone-since-m87.sh
@@ -15,4 +15,5 @@ pushd skia.git
     git branch | grep chrome/m1 | xargs -n 1 git fetch --update-shallow --shallow-since='Thu Oct 1 00:00:00 2020 +0000' origin
     git branch | grep chrome/m9 | xargs -n 1 git fetch --update-shallow --shallow-since='Thu Oct 1 00:00:00 2020 +0000' origin
     git branch | grep 'chrome/m8[789]' | xargs -n 1 git fetch --update-shallow --shallow-since='Thu Oct 1 00:00:00 2020 +0000' origin
+    git gc
 popd
