d1dd8e699e7577e05e410a2b7a01841afb42e1c3
Hin-Tak Leung <htl10@users.sourceforge.net>
2025-12-07 19:35:48 +0000

    Bump default to m144

--- download-and-build-skia.sh
+++ download-and-build-skia.sh
@@ -6,7 +6,7 @@
 
 ARG1=${1}
 
-VER=${ARG1:-m143}
+VER=${ARG1:-m144}
 
 # Shallow clones to avoid a 600MB+ download.
 git clone -b chrome/${VER} --depth 1 https://github.com/google/skia.git skia-${VER}
